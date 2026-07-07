110cb3ee897e67427b6cd624a768c6ff4a9bee2f
Gabrielly araujo <araujobatistagabrielly@gmail.com>
2026-07-06 22:37:19 -0300

    começado com javascript

---
+++ 01-variaveis.js
@@ -0,0 +1,7 @@
+// comentario do jsva
+// variavei são um espaço onde o programa guarda uma informação para poder usar depois
+// no js, temos 3 formas de declarar uma variavél
+
+// 1 - const - constante, não pode ser alterada depois de declarada
+const nome = "João";
+console.log(nome);
